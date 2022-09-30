b23dc8c1ea16b000d55f2a5154e0a4dcfb4ff97d
tohsaka888 <156132264@qq.com>
2022-09-30 17:24:39 +0800

    ✨ feat(hooks): 计算距离

---
+++ components/hooks/Node/useCalcDistence.tsx
@@ -0,0 +1,31 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-30 11:59:36
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-30 12:03:07
+ * @Description: 计算距离
+ */
+
+import { ConfigContext } from "components/context";
+import React, { useCallback, useContext } from "react";
+
+type Props = {
+  deg: number;
+  length: number;
+};
+
+function useCalcDistence() {
+  const { config } = useContext(ConfigContext)!;
+
+  const calcDistence = useCallback(
+    ({ deg, length }: Props) => {
+      const basicDistence = config.basicDistence;
+      return basicDistence + basicDistence * 0.1 * (30 / deg) * length;
+    },
+    [config.basicDistence]
+  );
+
+  return { calcDistence };
+}
+
+export default useCalcDistence;
