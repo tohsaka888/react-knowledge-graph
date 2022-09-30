bdef0df62284b8885a8d0a1629c96588c785a968
tohsaka888 <156132264@qq.com>
2022-09-30 17:24:00 +0800

    ✨ feat(config): baseURL

---
+++ config/baseUrl.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-30 13:03:20
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-30 13:03:20
+ * @Description: baseUrl
+ */
+
+export const baseUrl = `http://localhost:3000`;
