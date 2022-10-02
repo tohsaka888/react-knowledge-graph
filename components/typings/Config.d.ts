b732f94a624fe5307f9a841ef069b4131dedd09a
tohsaka888 <156132264@qq.com>
2022-10-02 10:19:08 +0800

    🦄 refactor: 重构typing

--- components/typings/Config.d.ts
+++ components/typings/Config.d.ts
@@ -7,6 +7,23 @@
  */
 
 declare namespace Config {
+  type NodeConfig = {
+    fill?: string; // 节点颜色
+    nameColor?: string; // 节点name颜色
+    typeColor?: string; // 节点类型颜色
+    nameSize?: number; // 节点名称文字大小
+    typeSize?: number; // 节点类型文字大小
+    hoveredColor?: string;
+  };
+
+  type EdgeConfig = {
+    stroke?: string;
+    strokeWidth?: number;
+    descriptionColor?: string;
+    descriptionSize?: number;
+    hoveredColor?: string;
+  };
+
   type ConfigProps = {
     radius: number;
     basicDistence: number; // 基础半径
@@ -17,5 +34,7 @@ declare namespace Config {
     }>;
     node: Node.NodeProps;
     position: { x: number; y: number };
+    nodeConfig?: NodeConfig;
+    edgeConfig?: EdgeConfig;
   };
 }
