8e312a329f3b3e3d9dfe77b9150abb7ec63d482c
tohsaka888 <156132264@qq.com>
2022-10-02 10:14:34 +0800

    ✨ feat: 边默认配置

---
+++ components/config/edgeConfig.ts
@@ -0,0 +1,7 @@
+export const defaultEdgeConfig: Required<Config.EdgeConfig> = {
+  strokeWidth: 1,
+  stroke: "#cecece",
+  hoveredColor: "#CCCCFF",
+  descriptionColor: "#cecece",
+  descriptionSize: 10,
+};
