d91f03a32f8d651ed8b9aaaef87fe1d71bbcb064
tohsaka888 <156132264@qq.com>
2022-10-02 10:14:50 +0800

    ✨ feat(config): 节点默认配置

---
+++ components/config/nodeConfig.ts
@@ -0,0 +1,8 @@
+export const defaultNodeConfig: Required<Config.NodeConfig> = {
+  fill: "#1890ff",
+  nameColor: "#666666",
+  typeColor: "#ffffff",
+  nameSize: 10,
+  typeSize: 10,
+  hoveredColor: '#ff0000'
+};
