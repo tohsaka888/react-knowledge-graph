172b9a346ce7f418e44cf180528702f54a6a9651
tohsaka888 <156132264@qq.com>
2022-10-03 18:28:13 +0800

    🦄 refactor: 重构Basel角度判断

--- components/Edge/index.tsx
+++ components/Edge/index.tsx
@@ -6,7 +6,7 @@
  * @Description: 边
  */
 
-import React, { useContext, useMemo } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import { motion, MotionConfig } from "framer-motion";
 import { ConfigContext, HoveredNodeContext } from "components/context";
 import { defaultEdgeConfig } from "components/config/edgeConfig";
@@ -29,40 +29,94 @@ function Edge({
       : false;
   }, [fromId, hoveredNode, toId]);
 
+  const calcQuadrant: Utils.CalcQuadrantFunc = ({ node, parentNode }) => {
+    // 为入边-二三象限
+    if (node.position.x - parentNode.position.x < 0) {
+      // 第二象限
+      if (node.position.y - parentNode.position.y < 0) {
+        return "第二象限";
+      }
+      // 第三象限
+      else {
+        return "第三象限";
+      }
+    }
+    // 为出边-一四象限
+    else {
+      if (node.position.y - parentNode.position.y < 0) {
+        return "第一象限";
+      } else {
+        return "第四象限";
+      }
+    }
+  };
+
+  const calcFlag: Utils.CalcFlagFunc = useCallback(
+    ({ fromNode, toNode, parentNode }) => {
+      const fromNodePosition = calcQuadrant({ node: fromNode, parentNode });
+      const toNodePosition = calcQuadrant({ node: toNode, parentNode });
+
+      if (fromNodePosition === "第一象限") {
+        if (toNodePosition === "第一象限") return 1;
+        else if (toNodePosition === "第二象限") return 1;
+        else if (toNodePosition === "第三象限") return 1;
+        else return -1;
+      } else if (fromNodePosition === "第二象限") {
+        if (toNodePosition === "第一象限") return -1;
+        else if (toNodePosition === "第二象限") return -1;
+        else if (toNodePosition === "第三象限") return -1;
+        else return -1;
+      } else if (fromNodePosition === "第三象限") {
+        if (toNodePosition === "第一象限") return 1;
+        else if (toNodePosition === "第二象限") return 1;
+        else if (toNodePosition === "第三象限") return 1;
+        else return -1;
+      } else {
+        if (toNodePosition === "第一象限") return 1;
+        else if (toNodePosition === "第二象限") return -1;
+        else if (toNodePosition === "第三象限") return 1;
+        else return 1;
+      }
+    },
+    []
+  );
+
   const d = useMemo(() => {
     const fromX = fromNode?.position.x as number;
     const fromY = fromNode?.position.y as number;
     const toX = toNode?.position.x as number;
     const toY = toNode?.position.y as number;
-    const fromParentY = fromNode?.parentNode?.position.y!;
-    const toParentY = toNode?.parentNode?.position.y!;
-    const flag =
-      (fromNode?.direction === "inside" && fromY - fromParentY > 0) ||
-      (toNode?.direction === "inside" &&
-        toY - toParentY > 0 &&
-        toY - fromY < 0) ||
-      toNode?.direction !== fromNode?.direction && (fromNode?.direction === 'outside')
-        ? 1
-        : -1;
-    // 中间点
-    const angle = Math.PI / 2 - Math.atan2(fromY - toY, toX - fromX);
-    const dx = flag * Math.cos(angle) * ((toX - fromX) / 2);
-    const dy = flag * Math.sin(angle) * ((toX - fromX) / 2 + 10);
-    const middlePoint = {
-      x: (fromX + toX + dx) / 2 + dx,
-      y: (fromY + toY) / 2 + dy,
-    };
-    return type === "straight"
-      ? `M ${fromX} ${fromY}, L ${toX} ${toY}`
-      : `M ${fromX} ${fromY}, Q ${middlePoint.x} ${middlePoint.y}, ${toX} ${toY}`;
+
+    if (type === "straight") {
+      return `M ${fromX} ${fromY}, L ${toX} ${toY}`;
+    } else {
+      const flag =
+        fromNode && toNode && fromNode.parentNode
+          ? calcFlag({
+              fromNode: fromNode!,
+              toNode: toNode!,
+              parentNode: fromNode?.parentNode!,
+            })
+          : 1;
+      // 中间点
+      const angle = Math.PI / 2 - Math.atan2(fromY - toY, toX - fromX);
+      const dx = flag * Math.cos(angle) * ((toX - fromX) / 2);
+      const dy = flag * Math.sin(angle) * ((toX - fromX) / 2 + 10);
+      const middlePoint = {
+        x: (fromX + toX + dx) / 2 + dx,
+        y: (fromY + toY) / 2 + dy,
+      };
+      return `M ${fromX} ${fromY}, Q ${middlePoint.x} ${middlePoint.y}, ${toX} ${toY}`;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
-    fromNode?.direction,
-    fromNode?.position.x,
-    fromNode?.position.y,
-    toNode?.direction,
-    toNode?.position.x,
-    toNode?.position.y,
+    fromNode,
+    toNode,
     type,
+    fromNode?.position.x,
+    toNode?.position.x,
+    fromNode?.position.y,
+    toNode?.position.y,
   ]);
 
   const {
@@ -104,7 +158,7 @@ function Edge({
                 d,
                 stroke: "#2890ff",
                 // opacity: needHighlight ? [0.5, 1] : 1,
-                strokeWidth: 2,
+                strokeWidth: 3,
                 pathSpacing: 1,
                 pathLength: [0, 0.5],
                 pathOffset: [0, 1],
