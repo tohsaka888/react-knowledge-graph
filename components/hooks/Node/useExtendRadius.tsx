ab116c86fd4794a632fd8715e4c5ca79e0cdf037
tohsaka888 <156132264@qq.com>
2022-10-03 18:29:04 +0800

    🦄 refactor: 重构延长半径

--- components/hooks/Node/useExtendRadius.tsx
+++ components/hooks/Node/useExtendRadius.tsx
@@ -9,22 +9,19 @@ type Props = {
 function useExtendRadius() {
   const calcNewPosition = useCallback(
     ({ node, insideLength, outsideLength }: Props) => {
-      const { position, direction, distence, angle } = node;
+      const { position, distence, angle } = node;
       let extendLength = 0;
       if (insideLength && outsideLength) {
         extendLength = 1.1 * distence;
       } else if (!insideLength && !outsideLength) {
         extendLength = 0;
       } else {
-        extendLength = 0.3 * distence;
+        extendLength = 0.5 * distence;
       }
 
       const x =
         position.x +
-        (!node.isExplore ? 1 : -1) *
-          (direction === "inside" ? 1 : -1) *
-          extendLength *
-          Math.sin(angle);
+        (!node.isExplore ? 1 : -1) * -extendLength * Math.sin(angle);
       const y =
         position.y +
         (!node.isExplore ? 1 : -1) * extendLength * Math.cos(angle);
