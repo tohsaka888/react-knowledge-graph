39edbd5dea64e4d1ceeddae078faec02f7597439
tohsaka888 <156132264@qq.com>
2022-10-03 08:58:33 +0800

    🐞 fix: 修复重复绘制边

--- components/hooks/Edge/useFormatEdges.tsx
+++ components/hooks/Edge/useFormatEdges.tsx
@@ -12,32 +12,50 @@ function useFormatEdges() {
   const { nodes } = useContext(NodesContext)!;
   const formatEdges = useCallback(
     ({ curNodes, edges, node }: Props) => {
-      setEdges((preEdges) => [
-        ...preEdges,
-        ...edges
-          .map((edge) => {
-            const fromNode = curNodes.find((node) => node.id === edge.fromId);
-            const toNode = curNodes.find((node) => node.id === edge.toId);
-            return {
-              ...edge,
-              type:
-                (node.id === edge.fromId &&
-                  !nodes.find((n) => n.id === edge.toId && n.id !== node.id)) ||
-                (node.id === edge.toId &&
-                  !nodes.find((n) => n.id === edge.fromId && n.id !== node.id))
-                  ? ("straight" as "straight")
-                  : ("curve" as "curve"),
-              pId: [...(fromNode?.pId || []), ...(toNode?.pId || [])],
-              fromNode,
-              toNode,
-            };
-          })
-          .filter(
-            (edge) =>
-              !(edge.fromId === node.id && edge.toId === node.parentNode?.id) &&
-              !(edge.toId === node.id && edge.fromId === node.parentNode?.id)
-          ),
-      ]);
+      setEdges((preEdges) => {
+        // 去重
+        const filteredEdges = edges.filter(
+          (edge) => !preEdges.find((preEdge) => preEdge.id === edge.id)
+        );
+        return Array.from(
+          new Set([
+            ...preEdges,
+            ...filteredEdges
+              .map((edge) => {
+                const fromNode = curNodes.find(
+                  (node) => node.id === edge.fromId
+                );
+                const toNode = curNodes.find((node) => node.id === edge.toId);
+                return {
+                  ...edge,
+                  type:
+                    (node.id === edge.fromId &&
+                      !nodes.find(
+                        (n) => n.id === edge.toId && n.id !== node.id
+                      )) ||
+                    (node.id === edge.toId &&
+                      !nodes.find(
+                        (n) => n.id === edge.fromId && n.id !== node.id
+                      ))
+                      ? ("straight" as "straight")
+                      : ("curve" as "curve"),
+                  pId: [...(fromNode?.pId || []), ...(toNode?.pId || [])],
+                  fromNode,
+                  toNode,
+                };
+              })
+              .filter(
+                (edge) =>
+                  !(
+                    edge.fromId === node.id && edge.toId === node.parentNode?.id
+                  ) &&
+                  !(
+                    edge.toId === node.id && edge.fromId === node.parentNode?.id
+                  )
+              ),
+          ])
+        );
+      });
     },
     [nodes, setEdges]
   );
