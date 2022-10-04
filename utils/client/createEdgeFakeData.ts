efb2d67085439d4e1d7a668d72f1a619753c46a4
tohsaka888 <156132264@qq.com>
2022-10-04 11:43:18 +0800

    ✨ feat(utils): 生成边假数据

---
+++ utils/client/createEdgeFakeData.ts
@@ -0,0 +1,31 @@
+type Props = {
+  direction: "inside" | "outside";
+  length: number;
+  rootId: string;
+  edgeLastId: number;
+};
+
+type CreateEdgeFakeDataFunc = ({
+  direction,
+  length,
+  rootId,
+  edgeLastId,
+}: Props) => Edge.EdgeProps[];
+
+export const createEdgeFakeData: CreateEdgeFakeDataFunc = ({
+  direction,
+  length,
+  rootId,
+  edgeLastId,
+}) => {
+  const edges: Edge.EdgeProps[] = [];
+  for (let i = 0; i < length; i++) {
+    edges.push({
+      id: "edge-" + (edgeLastId + i + 1),
+      fromId: direction === "outside" ? rootId : rootId + "-0" + (i + 1),
+      toId: direction === "inside" ? rootId : rootId + "-1" + (i + 1),
+      description: "description",
+    });
+  }
+  return edges;
+};
