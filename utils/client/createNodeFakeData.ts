73a4e71cdb0757a7f285828081016811671b0249
tohsaka888 <156132264@qq.com>
2022-10-04 11:43:32 +0800

    ✨ feat(utils): 生成节点假数据

---
+++ utils/client/createNodeFakeData.ts
@@ -0,0 +1,40 @@
+type Props = {
+  direction: "inside" | "outside";
+  length: number;
+  rootId: string;
+};
+
+type CreateNodeFakeDataFunc = ({
+  direction,
+  length,
+  rootId,
+}: Props) => (Node.NodeProps & { parentId: string })[];
+
+export const createNodeFakeData: CreateNodeFakeDataFunc = ({
+  direction,
+  length,
+  rootId,
+}) => {
+  const nodes: (Node.NodeProps & { parentId: string })[] = [];
+
+  for (let i = 0; i < length; i++) {
+    const random = Math.random();
+    let type = "";
+    if (random >= 0 && random < 0.3) {
+      type = "test";
+    } else if (random >= 0.3 && random < 0.6) {
+      type = "model";
+    } else {
+      type = "data";
+    }
+    nodes.push({
+      id: direction === 'inside' ? rootId + "-0" + (i + 1) : rootId + "-1" + (i + 1),
+      type,
+      name: "测试节点",
+      hasMore: true,
+      direction,
+      parentId: rootId,
+    });
+  }
+  return nodes;
+};
