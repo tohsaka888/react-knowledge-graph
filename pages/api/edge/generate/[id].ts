b683b0da40871ece26a9067d9863804144b9ccbd
tohsaka888 <156132264@qq.com>
2022-10-04 11:41:35 +0800

    ✨ feat(api): 自动生成边数据

---
+++ pages/api/edge/generate/[id].ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createEdgeFakeData } from "utils/client/createEdgeFakeData";
+import { connectDB } from "utils/server/connectDB";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const db = await connectDB();
+    const id = req.query.id as string;
+    const insideLength = req.query.insideLength as string;
+    const outsideLength = req.query.outsideLength as string;
+
+    const collection = await db.collection("Edges");
+    const lastData = await collection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+    const lastEdgeId = +lastData[0].id.split("-")[1];
+    const insdieEdgeFakeData = createEdgeFakeData({
+      rootId: id,
+      direction: "inside",
+      length: +insideLength,
+      edgeLastId: lastEdgeId,
+    });
+    const outsideEdgeFakeData = createEdgeFakeData({
+      rootId: id,
+      direction: "outside",
+      length: +outsideLength,
+      edgeLastId: lastEdgeId + +insideLength,
+    });
+    collection.insertMany([...insdieEdgeFakeData, ...outsideEdgeFakeData])
+    if (db) {
+      res
+        .status(200)
+        .send({
+          insideEdges: insdieEdgeFakeData,
+          outsideEdges: outsideEdgeFakeData,
+        });
+    }
+  } catch (error) {
+    res.status(200).send([]);
+  }
+}
