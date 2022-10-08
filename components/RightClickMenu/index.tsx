c234061879e6bc13e9b3f31a3f5f46ce5b54b790
tohsaka888 <156132264@qq.com>
2022-10-08 15:06:43 +0800

    ✨ feat: 右键菜单

---
+++ components/RightClickMenu/index.tsx
@@ -0,0 +1,90 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-10-08 13:20:39
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-10-08 15:06:23
+ * @Description: 右键菜单
+ */
+
+import React, { useContext, useEffect, useMemo, useRef } from "react";
+import ReactDOM from "react-dom";
+import { motion } from "framer-motion";
+import { RightMenuPropsContext } from "components/context";
+import MenuItem from "./MenuItem";
+import useCanvasDragOrScale from "components/hooks/Canvas/useCanvasDragOrScale";
+
+const canvasItems = ["复位画布", "下载当前图谱", "全屏"];
+
+function RightClickMenu() {
+  const bodyRef = useRef<HTMLElement>(null!);
+  const { event, setEvent } = useContext(RightMenuPropsContext)!;
+  const { resetCanvas } = useCanvasDragOrScale();
+
+  useEffect(() => {
+    bodyRef.current = document.body;
+  }, [setEvent]);
+
+  const type = useMemo(() => {
+    if (event?.target) {
+      const target = event.target as any;
+      if (target.className.baseVal.includes("canvas")) {
+        return "canvas";
+      } else if (target.className.baseVal.includes("node")) {
+        return "node";
+      } else {
+        return "canvas";
+      }
+    }
+  }, [event]);
+
+  return (
+    bodyRef.current &&
+    ReactDOM.createPortal(
+      <>
+        {event && (
+          <motion.div
+            id={"right-menu"}
+            initial={{
+              position: "fixed",
+              willChange: "transform",
+              top: "0px",
+              left: "0px",
+              opacity: 0,
+              transform: `translate3d(${event.clientX}px, ${event.clientY}px, 0)`,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+          >
+            {type === "canvas" ? (
+              <motion.div initial={{ border: "1px solid #dfdfdf" }}>
+                {canvasItems.map((item, index) => {
+                  return (
+                    <MenuItem
+                      index={index}
+                      length={canvasItems.length}
+                      key={item}
+                      onClick={(value) => {
+                        if (value === "复位画布") {
+                          resetCanvas();
+                        }
+                        setEvent(null);
+                      }}
+                    >
+                      {item}
+                    </MenuItem>
+                  );
+                })}
+              </motion.div>
+            ) : (
+              <motion.div>node</motion.div>
+            )}
+          </motion.div>
+        )}
+      </>,
+      bodyRef.current
+    )
+  );
+}
+
+export default RightClickMenu;
