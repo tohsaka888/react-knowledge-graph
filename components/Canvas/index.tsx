f07c08f9c5c0362f5f83006bb77475d12f5b22c7
tohsaka888 <156132264@qq.com>
2022-10-08 15:05:37 +0800

    ✨ feat: 新增画布右键菜单

--- components/Canvas/index.tsx
+++ components/Canvas/index.tsx
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-09-30 09:04:35
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-10-08 12:28:35
+ * @LastEditTime: 2022-10-08 14:27:43
  * @Description: 请填写简介
  */
 
@@ -13,9 +13,11 @@ import {
   // EdgeMenuContext,
   EdgesContext,
   NodesContext,
+  RightMenuPropsContext,
 } from "components/context";
 import Edge from "components/Edge";
 import EdgeMenu from "components/Edge/EdgeMenu";
+import RightClickMenu from "components/RightClickMenu";
 
 // function EdgeController({ children }: { children: React.ReactNode }) {
 //   const [isHovered, setIsHovered] = useState<boolean>(false);
@@ -32,29 +34,39 @@ import EdgeMenu from "components/Edge/EdgeMenu";
 function Canvas() {
   const { nodes } = useContext(NodesContext)!;
   const { edges } = useContext(EdgesContext)!;
+  const { setEvent } = useContext(RightMenuPropsContext)!;
   return (
-    <motion.svg
-      width={"100%"}
-      height={"100%"}
-      id={"knowledge-graph-canvas"}
-      style={{ userSelect: "none" }}
-    >
-      <motion.g id={"graph-drag"}>
-        <motion.g id={"graph-scale"}>
-          {/* <EdgeController> */}
-          {edges.map((edge) => {
-            return <Edge {...edge} key={edge.id} />;
-          })}
-          {edges.map((edge) => {
-            return <EdgeMenu edge={edge} key={edge.id} />;
-          })}
-          {nodes.map((node) => {
-            return <Node node={node} key={node.id} />;
-          })}
-          {/* </EdgeController> */}
+    <>
+      <motion.svg
+        width={"100%"}
+        height={"100%"}
+        id={"knowledge-graph-canvas"}
+        style={{ userSelect: "none" }}
+        onContextMenu={(e) => {
+          e.preventDefault();
+          setEvent(e);
+        }}
+        className={"canvas"}
+        onClick={() => {
+          setEvent(null);
+        }}
+      >
+        <motion.g id={"graph-drag"}>
+          <motion.g id={"graph-scale"}>
+            {edges.map((edge) => {
+              return <Edge {...edge} key={edge.id} />;
+            })}
+            {edges.map((edge) => {
+              return <EdgeMenu edge={edge} key={edge.id} />;
+            })}
+            {nodes.map((node) => {
+              return <Node node={node} key={node.id} />;
+            })}
+          </motion.g>
         </motion.g>
-      </motion.g>
-    </motion.svg>
+      </motion.svg>
+      <RightClickMenu />
+    </>
   );
 }
 
