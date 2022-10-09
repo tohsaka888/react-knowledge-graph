5f7ac0e906f38c2b72d1c2117e00ab34f96e6127
tohsaka888 <156132264@qq.com>
2022-10-09 17:09:12 +0800

    🦄 refactor: 入口文件

--- KnowledgeGraph/index.tsx
+++ KnowledgeGraph/index.tsx
@@ -1,6 +1,6 @@
 import KnowledgeGraph from "./KnowledgeGraph";
-
 export default KnowledgeGraph;
+
 export * from "./typings/Canvas";
 export * from "./typings/Config";
 export * from "./typings/Edge";
