89b788081796ecf757538631ca3a3f6ec7e9bb7e
tohsaka888 <156132264@qq.com>
2022-10-09 17:09:28 +0800

    🦄 refactor: remove doc

--- KnowledgeGraph/Canvas/index.tsx
+++ KnowledgeGraph/Canvas/index.tsx
@@ -2,34 +2,17 @@
  * @Author: tohsaka888
  * @Date: 2022-09-30 09:04:35
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-10-09 14:12:44
+ * @LastEditTime: 2022-10-09 16:25:30
  * @Description: 请填写简介
  */
 
 import React, { useContext } from "react";
 import { motion } from "framer-motion";
 import Node from "../Node";
-import {
-  // EdgeMenuContext,
-  EdgesContext,
-  NodesContext,
-  RightMenuPropsContext,
-} from "../context";
+import { EdgesContext, NodesContext, RightMenuPropsContext } from "../context";
 import Edge from "../Edge";
 import EdgeMenu from "../Edge/EdgeMenu";
 import RightClickMenu from "../RightClickMenu";
-
-// function EdgeController({ children }: { children: React.ReactNode }) {
-//   const [isHovered, setIsHovered] = useState<boolean>(false);
-//   const [isShow, setIsShow] = useState<boolean>(true);
-//   return (
-//     <EdgeMenuContext.Provider
-//       value={{ isHovered, isShow, setIsHovered, setIsShow }}
-//     >
-//       {children}
-//     </EdgeMenuContext.Provider>
-//   );
-// }
 
 function Canvas() {
   const { nodes } = useContext(NodesContext)!;
