9100b3b69a8d8af5a8d2ab704d6d0de8ba03090d
tohsaka888 <156132264@qq.com>
2022-10-09 17:10:21 +0800

    ✨ feat: 更改默认配置

--- KnowledgeGraph/config/nodeConfig.ts
+++ KnowledgeGraph/config/nodeConfig.ts
@@ -1,8 +1,13 @@
-export const defaultNodeConfig = {
+import { NodeConfig } from "KnowledgeGraph";
+
+export const defaultNodeConfig: Required<NodeConfig> = {
   fill: "#1890ff",
   nameColor: "#666666",
   typeColor: "#ffffff",
   nameSize: 10,
   typeSize: 10,
-  hoveredColor: '#ff0000'
+  hoverStyle: {
+    fill: "#23ff45",
+  },
+  radius: 20,
 };
