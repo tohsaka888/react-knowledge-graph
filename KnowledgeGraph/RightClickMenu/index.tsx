162e3728df544868a5fbc64c0c4dba350da7985c
tohsaka888 <156132264@qq.com>
2022-10-11 14:54:05 +0800

    🎈 perf: 优化性能

--- KnowledgeGraph/RightClickMenu/index.tsx
+++ KnowledgeGraph/RightClickMenu/index.tsx
@@ -25,20 +25,10 @@ import MenuItem from "./MenuItem";
 const canvasItems = ["复位画布", "下载当前图谱", "全屏"];
 const nodeItems = ["当前实体居中"];
 
-function RightClickMenu() {
-  const bodyRef = useRef<HTMLElement>(null!);
-  const { event, setEvent } = useContext(RightMenuPropsContext)!;
+function RightMenuContent() {
   const { nodes } = useContext(NodesContext)!;
   const { setCanvasConfig } = useContext(CanvasConfigContext)!;
-
-  useEffect(() => {
-    bodyRef.current = document.body;
-
-    return () => {
-      setEvent(null);
-    };
-  }, [setEvent]);
-
+  const { event, setEvent } = useContext(RightMenuPropsContext)!;
   const type = useMemo(() => {
     if (event?.target) {
       const target = event.target as any;
@@ -61,86 +51,98 @@ function RightClickMenu() {
   }, [setCanvasConfig]);
 
   return (
-    bodyRef.current &&
-    ReactDOM.createPortal(
-      <>
-        {event && (
-          <motion.div
-            id={"right-menu"}
-            initial={{
-              position: "fixed",
-              willChange: "transform",
-              top: "0px",
-              left: "0px",
-              opacity: 0,
-              transform: `translate3d(${event.clientX}px, ${event.clientY}px, 0)`,
-            }}
-            animate={{
-              opacity: 1,
-              transform: `translate3d(${event.clientX}px, ${event.clientY}px, 0)`,
-            }}
-          >
-            {type === "canvas" ? (
-              <motion.div initial={{ border: "1px solid #dfdfdf" }}>
-                {canvasItems.map((item, index) => {
-                  return (
-                    <MenuItem
-                      index={index}
-                      length={canvasItems.length}
-                      key={item}
-                      onClick={(value) => {
-                        if (value === "复位画布") {
-                          resetCanvas();
-                        }
-                        if (value === "全屏" || value === "退出全屏") {
-                          if (document.fullscreenElement) {
-                            document.exitFullscreen();
-                          } else {
-                            document.documentElement.requestFullscreen();
-                          }
-                        }
-                        setEvent(null);
-                      }}
-                    >
-                      {item === "全屏"
-                        ? !document.fullscreenElement
-                          ? "全屏"
-                          : "退出全屏"
-                        : item}
-                    </MenuItem>
-                  );
-                })}
-              </motion.div>
-            ) : (
-              <motion.div initial={{ border: "1px solid #dfdfdf" }}>
-                {nodeItems.map((item, index) => {
-                  return (
-                    <MenuItem
-                      key={item}
-                      index={index}
-                      length={nodeItems.length}
-                      onClick={() => {
-                        if (item === "当前实体居中") {
-                          const nodeId = (
-                            event.target as HTMLElement
-                          ).getAttribute("node-id");
-                          const node = nodes.find((n) => n.id === nodeId)!;
-                          // moveNodeToCenter(node);
-                        }
-                        setEvent(null);
-                      }}
-                    >
-                      {item}
-                    </MenuItem>
-                  );
-                })}
-              </motion.div>
-            )}
+    <>
+      <motion.div
+        id={"right-menu"}
+        initial={{
+          position: "fixed",
+          willChange: "transform",
+          top: "0px",
+          left: "0px",
+          opacity: 0,
+          transform: `translate3d(${event!.clientX}px, ${event!.clientY}px, 0)`,
+        }}
+        animate={{
+          opacity: 1,
+          transform: `translate3d(${event!.clientX}px, ${event!.clientY}px, 0)`,
+        }}
+      >
+        {type === "canvas" ? (
+          <motion.div initial={{ border: "1px solid #dfdfdf" }}>
+            {canvasItems.map((item, index) => {
+              return (
+                <MenuItem
+                  index={index}
+                  length={canvasItems.length}
+                  key={item}
+                  onClick={(value) => {
+                    if (value === "复位画布") {
+                      resetCanvas();
+                    }
+                    if (value === "全屏" || value === "退出全屏") {
+                      if (document.fullscreenElement) {
+                        document.exitFullscreen();
+                      } else {
+                        document.documentElement.requestFullscreen();
+                      }
+                    }
+                    setEvent(null);
+                  }}
+                >
+                  {item === "全屏"
+                    ? !document.fullscreenElement
+                      ? "全屏"
+                      : "退出全屏"
+                    : item}
+                </MenuItem>
+              );
+            })}
+          </motion.div>
+        ) : (
+          <motion.div initial={{ border: "1px solid #dfdfdf" }}>
+            {nodeItems.map((item, index) => {
+              return (
+                <MenuItem
+                  key={item}
+                  index={index}
+                  length={nodeItems.length}
+                  onClick={() => {
+                    if (item === "当前实体居中") {
+                      const nodeId = (
+                        event!.target as HTMLElement
+                      ).getAttribute("node-id");
+                      const node = nodes.find((n) => n.id === nodeId)!;
+                      // moveNodeToCenter(node);
+                    }
+                    setEvent(null);
+                  }}
+                >
+                  {item}
+                </MenuItem>
+              );
+            })}
           </motion.div>
         )}
-      </>,
-      bodyRef.current
-    )
+      </motion.div>
+    </>
+  );
+}
+
+function RightClickMenu() {
+  const bodyRef = useRef<HTMLElement>(null!);
+  const { event, setEvent } = useContext(RightMenuPropsContext)!;
+
+  useEffect(() => {
+    bodyRef.current = document.body;
+
+    return () => {
+      setEvent(null);
+    };
+  }, [setEvent]);
+
+  return (
+    bodyRef.current &&
+    ReactDOM.createPortal(<>{event && <RightMenuContent />}</>, bodyRef.current)
   );
 }
 
