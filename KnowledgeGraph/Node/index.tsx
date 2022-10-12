40db55d1e8f9adec6c8fa0a878b2688de32584e4
tohsaka888 <156132264@qq.com>
2022-10-12 13:01:21 +0800

    🦄 refactor: 提取explore函数

--- KnowledgeGraph/Node/index.tsx
+++ KnowledgeGraph/Node/index.tsx
@@ -20,24 +20,16 @@ import Loading from "./Loading";
 import { defaultNodeConfig } from "../config/nodeConfig";
 import useFormatEdges from "../hooks/Edge/useFormatEdges";
 import { NodeFrontProps } from "KnowledgeGraph";
+import useExplore from "./useExplore";
 
 function UnmemoNode({ node }: { node: NodeFrontProps }) {
   const { config } = useContext(ConfigContext)!;
   const { explore, typeConfig, onExploreEnd } = config;
   const { name, type, position, parentNode } = node;
   const nodeConfig = typeConfig && typeConfig[type];
-  const { calcNodePosition } = useNodePosition();
-  const { nodes, setNodes } = useContext(NodesContext)!;
-  const { setEdges } = useContext(EdgesContext)!;
-  const { calcNewPosition } = useExtendRadius();
-  const [loading, setLoading] = useState<boolean>(false);
+
   const [isHover, setIsHover] = useState<boolean>(false);
   const { setHoveredNode } = useContext(HoveredNodeContext)!;
-  const { formatEdges } = useFormatEdges();
-  const [childNodeLength, setChildNodeLength] = useState<{
-    insideLength: number;
-    outsideLength: number;
-  }>({ insideLength: 0, outsideLength: 0 });
   const fill = nodeConfig?.fill || defaultNodeConfig.fill;
   const hoverStyle = nodeConfig?.hoverStyle || defaultNodeConfig.hoverStyle;
   const nameColor = nodeConfig?.nameColor || defaultNodeConfig.nameColor;
@@ -46,92 +38,7 @@ function UnmemoNode({ node }: { node: NodeFrontProps }) {
   const typeSize = nodeConfig?.typeSize || defaultNodeConfig.typeSize;
   const radius = nodeConfig?.radius || defaultNodeConfig.radius;
 
-  const exploreFunc = async () => {
-    setLoading(true);
-
-    // 判断当前节点是否已探索
-    if (!node.isExplore) {
-      const { inside, outside, edges } = await explore(node.id);
-      setChildNodeLength({
-        insideLength: inside.length,
-        outsideLength: outside.length,
-      });
-      // 探索-延长半径 取消探索-缩短半径
-      calcNewPosition({
-        node,
-        insideLength: inside.length,
-        outsideLength: outside.length,
-      });
-      // 判断是否有子节点
-      if (node.hasMore) {
-        const frontInside = calcNodePosition({
-          direction: "inside",
-          nodes: inside,
-          parentNode: node,
-          insideLength: inside.length,
-          outsideLength: outside.length,
-        });
-        const frontOutside = calcNodePosition({
-          direction: "outside",
-          nodes: outside,
-          parentNode: node,
-          insideLength: inside.length,
-          outsideLength: outside.length,
-        });
-        const curNodes = [
-          ...nodes.map((current) => {
-            if (current.id === node.id) {
-              return {
-                ...current,
-                isExplore: true,
-              };
-            } else {
-              return current;
-            }
-          }),
-          ...frontInside,
-          ...frontOutside,
-        ];
-        setNodes(curNodes);
-        // 设置边
-        formatEdges({ node, curNodes, edges });
-      } else {
-        onExploreEnd && onExploreEnd();
-      }
-    } else {
-      // 探索-延长半径 取消探索-缩短半径
-      calcNewPosition({
-        node,
-        ...childNodeLength,
-      });
-      // 过滤掉子节点
-      const filteredNodes = nodes.filter(
-        (current) => !current.pId.find((id) => id === node.id)
-      );
-      setNodes(
-        filteredNodes.map((filteredNode) => {
-          if (filteredNode.id === node.id) {
-            return {
-              ...filteredNode,
-              isExplore: false,
-            };
-          } else {
-            return filteredNode;
-          }
-        })
-      );
-      // 过滤边
-      setEdges((edges) => {
-        return edges.filter(
-          (edge) =>
-            filteredNodes.find((node) => node.id === edge.fromId) &&
-            filteredNodes.find((node) => node.id === edge.toId)
-        );
-      });
-    }
-    node.isExplore = !node.isExplore;
-    setLoading(false);
-  };
+  const { exploreFunc, loading } = useExplore({ explore, node, onExploreEnd });
 
   return (
     <AnimatePresence>
