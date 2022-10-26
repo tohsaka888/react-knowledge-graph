4e56861a6b7cb97fee7d86a9bb9e41014f8784d2
tohsaka888 <156132264@qq.com>
2022-10-26 14:51:08 +0800

    ✨ feat: use custom hook

--- KnowledgeGraph/RightClickMenu/index.tsx
+++ KnowledgeGraph/RightClickMenu/index.tsx
@@ -27,7 +27,7 @@ import {
   moveCanvasToPosition,
   resetCanvas,
 } from "../Controller/canvasConfigSlice";
-import { FullScreenContext } from "../Controller/FullScreenController";
+import { useDispatchFullScreen } from "../Controller/FullScreenController";
 
 const canvasItems = ["复位画布", "下载当前图谱", "全屏"];
 const nodeItems = ["当前实体居中", "显示当前节点关系", "显示所有节点"];
@@ -111,7 +111,7 @@ function RightMenuContent() {
   );
 
   const [isDownload, setIsDownload] = useState<boolean>(false);
-  const { setIsFullScreen } = useContext(FullScreenContext)!;
+  const dispatchFullScreen = useDispatchFullScreen();
 
   return (
     <>
@@ -147,10 +147,10 @@ function RightMenuContent() {
                         if (value === "全屏" || value === "退出全屏") {
                           if (document.fullscreenElement) {
                             document.exitFullscreen();
-                            setIsFullScreen(false);
+                            dispatchFullScreen(false);
                           } else {
                             document.documentElement.requestFullscreen();
-                            setIsFullScreen(true);
+                            dispatchFullScreen(true);
                           }
                           setEvent(null);
                         }
